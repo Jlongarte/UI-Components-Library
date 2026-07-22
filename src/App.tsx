673e536a6c875066bc60c9b7c4456f1cb2c13d8b
Jlongarte <longarte.janire@gmail.com>
2026-07-22 09:45:08 +0200

    Custom Cursor 1

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import CustomCursor1 from './components/Cursors/CustomCursor1';
+
+
+export const App = () => {
+  return (
+    <main >
+      <CustomCursor1 text="• DEVELOPER • FRONTend • REACT • " spinDuration={8} />
+    </main>
+  );
+};
---
+++ src/components/Cursors/CustomCursor1.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from 'react';
+import '../../styles/CustomCursor1.css';
+
+
+interface CustomCursorProps {
+  text?: string;
+  spinDuration?: number;
+}
+
+export const CustomCursor: React.FC<CustomCursorProps> = ({
+  text = '• DISEÑO • FULLSTACK • DEVELOPER ', // Valor por defecto
+  spinDuration = 12,
+}) => {
+  const [position, setPosition] = useState({ x: -100, y: -100 });
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+      if (!isVisible) setIsVisible(true);
+    };
+
+    const handleMouseLeave = () => {
+      setIsVisible(false);
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mouseleave', handleMouseLeave);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+      document.removeEventListener('mouseleave', handleMouseLeave);
+    };
+  }, [isVisible]);
+
+  return (
+    <div
+      className="custom-cursor-container"
+      style={{
+        transform: `translate3d(${position.x}px, ${position.y}px, 0)`,
+        opacity: isVisible ? 1 : 0,
+      }}
+    >
+      <div className="custom-cursor-wrapper">
+        <svg
+          viewBox="0 0 100 100"
+          className="custom-cursor-svg"
+          style={{ animationDuration: `${spinDuration}s` }}
+        >
+          <path
+            id="circlePath"
+            d="M 50, 50 m -37, 0 a 37,37 0 1,1 74,0 a 37,37 0 1,1 -74,0"
+            fill="none"
+          />
+          <text className="custom-cursor-text">
+            {/* Renderizamos la prop dinámicamente */}
+            <textPath href="#circlePath">{text}</textPath>
+          </text>
+        </svg>
+
+        <div className="custom-cursor-dot" />
+      </div>
+    </div>
+  );
+};
+
+export default CustomCursor;
